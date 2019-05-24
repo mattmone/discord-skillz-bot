824bbc1745d61852acfee9bc3550fb957dbdc49a
Matthew Mone <matthew.mone@gcu.edu>
2019-05-24 07:32:38 -0700

    initial commit - base bot

---
+++ index.js
@@ -0,0 +1,66 @@
+const Discord = require("discord.io");
+const logger = require("winston");
+const auth = require("./auth.json");
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console(), {
+  colorize: true
+});
+logger.level = "debug";
+// Initialize Discord Bot
+const bot = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+let servers = {};
+bot.on("ready", function(event) {
+  logger.info("Connected");
+  logger.info("Logged in as: ");
+  logger.info(bot.username + " - (" + bot.id + ")");
+  for(server in bot.servers) {
+    if(!servers[server]) servers[server] = {};
+    servers[server].members = Object.assign(bot.servers[server].members);
+  }
+  logger.info(servers);
+});
+bot.on("message", function(user, userID, channelID, message, event) {
+  //listen for bot mention
+  if (message.indexOf(`<@${bot.id}>`) === -1) return;
+  let commandMatch = /(new skill|check skill|level up) (.+)/gi.exec(message);
+  if(commandMatch === null) return;
+  commandMatch.shift();
+  const [command, args] = commandMatch;
+  console.log(command, args);
+  if(!commands[command]) return bot.sendMessage({to: channelID, message: `sorry, I don't understand.`});
+  commands[command](event.d.guild_id, channelID, args);
+});
+
+const commands = {
+  "new skill": newSkill,
+  "check skill": checkSkill,
+  "level up": levelUp
+};
+function levelUp(server, channelID, args) {
+  const [skill, member] = args.split(' ');
+  const memberid = member.replace('<@','').replace('>','');
+  servers[server].skills[skill][memberid]++;
+  bot.sendMessage({to: channelID, message: `${skill} for ${member} is now ${servers[server].skills[skill][memberid]}`});
+}
+function checkSkill(server, channelID, args) {
+  const [skill, member] = args.split(' ');
+  const memberid = member.replace('<@','').replace('>','');
+  bot.sendMessage({to: channelID, message: `${skill} for ${member} is ${servers[server].skills[skill][memberid]}`});
+}
+function newSkill(server, channelID, skill) {
+  if(!servers[server].skills) servers[server].skills = {};
+  if(servers[server].skills[skill]) return bot.sendMessage({to: channelID, message: `We've already got a skill named ${skill}`});
+  servers[server].skills[skill] = {};
+  for(member of Object.keys(servers[server].members)) {
+    if(member !== bot.id)
+      servers[server].skills[skill][member] = Math.floor(Math.random()*4)+1;
+  }
+  let message = `adding the skill ${skill}\n`;
+  for(member of Object.keys(servers[server].skills[skill]))
+    message += `${servers[server].members[member].username}: ${servers[server].skills[skill][member]}\n`
+  bot.sendMessage({to: channelID, message: message});
+}
